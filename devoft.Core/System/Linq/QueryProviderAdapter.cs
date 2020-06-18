b81df49e6ca009fd5219eabd3d21ced2813e8e28
Mario del Valle Matos <mario@ILITIA.COM>
2020-06-18 11:31:42 +0200

    Linq Query Adapters

---
+++ devoft.Core/System/Linq/QueryProviderAdapter.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+
+namespace devoft.System.Linq
+{
+    /// <summary>
+    /// Base class of QueryProvider Adapters. This providers works with <see cref="QueryableAdapter{TElement}"/>
+    /// </summary>
+    /// <typeparam name="TQueryable"></typeparam>
+    /// <typeparam name="TElement"></typeparam>
+    public class QueryProviderAdapter<TQueryable, TElement> : IQueryProvider
+        where TQueryable : QueryableAdapter<TElement>
+    {
+        protected IQueryProvider _wrapped;
+
+        protected internal QueryProviderAdapter(IQueryProvider wrapped)
+        {
+            _wrapped = wrapped;
+        }
+
+        public virtual IQueryable<TItem> CreateQuery<TItem>(Expression expression)
+            => new QueryableAdapter<TItem>(_wrapped.CreateQuery<TItem>(expression));
+
+        public IQueryable CreateQuery(Expression expression)
+            => CreateQuery<TElement>(expression);
+
+        public virtual TResult Execute<TResult>(Expression expression)
+            => (TResult)_wrapped.CreateQuery<TElement>(expression);
+
+        public object Execute(Expression expression)
+            => Execute<object>(expression);
+    }
+
+
+}
---
+++ devoft.Core/System/Linq/QueryableAdapter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace devoft.System.Linq
+{
+    /// <summary>
+    /// Base class for Queryable adapters. This queryables combines with <see cref="QueryProviderAdapter{TQueryable, TElement}"/>
+    /// </summary>
+    /// <typeparam name="TElement"></typeparam>
+    public class QueryableAdapter<TElement> : IOrderedQueryable<TElement>
+    {
+        protected IQueryable<TElement> _query;
+
+        public QueryableAdapter(IQueryable<TElement> query)
+        {
+            _query = query;
+            Provider = BuildQueryProviderOverride(_query.Provider);
+        }
+
+        public QueryableAdapter(IQueryable query) : this((IQueryable<TElement>)query) { }
+
+        public Type ElementType
+            => typeof(TElement);
+
+        public virtual Expression Expression
+            => _query.Expression;
+
+        public virtual IQueryProvider Provider { get; }
+
+        public IEnumerator<TElement> GetEnumerator()
+            => Provider.Execute<IQueryable<TElement>>(Expression).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        protected virtual IQueryProvider BuildQueryProviderOverride(IQueryProvider wrapped)
+            => new QueryProviderAdapter<QueryableAdapter<TElement>, TElement>(wrapped);
+    }
+
+
+}
